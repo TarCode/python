737946ccd6da31190ddc1226cb959c60894762e6
TarCode <TarCode@github.com>
2015-06-15 22:16:45 +0200

    python practice initial commit with example 1

---
+++ example1.py
@@ -0,0 +1,11 @@
+tunes = {
+	"Berg" : "Stranger to yourself",
+	"Lish" : "The 5th Tone",
+	"Zyce and Aquafeel" : "Dark Side"
+	}
+
+def printTunes(tunes):
+	for key in tunes:
+		print key, tunes[key]
+
+printTunes(tunes)
